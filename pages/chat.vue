ee055b2704c47592ed29badb318280dc21f9976b
Capchen <chenjian@gyenno.com>
2023-03-13 11:22:45 +0800

    feat(): 增加聊天页

---
+++ pages/chat.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="overflow-hidden relative w-screen h-screen flex flex-col">
+  <div class="overflow-y-auto flex-auto flex flex-col items-center text-sm dark:bg-gray-800">
+    <div class="group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 dark:bg-gray-800">
+      <div class="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto">
+        <img class="h-8 w-8 rounded-full" src="https://via.placeholder.com/150" alt="">
+        <p class="relative flex w-[calc(100%-50px)] flex-col gap-1 md:gap-3 lg:w-[calc(100%-115px)]">这是一条聊天消息</p>
+      </div>
+    </div>
+    <div class="group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 bg-gray-50 dark:bg-[#444654]">
+      <div class="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto">
+        <img class="h-8 w-8 rounded-full" src="https://via.placeholder.com/150" alt="">
+        <p class="relative flex w-[calc(100%-50px)] flex-col gap-1 md:gap-3 lg:w-[calc(100%-115px)]">这是一条聊天消息</p>
+      </div>
+    </div>
+  </div>
+  <div class="h-30 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient">
+    <form class="stretch mx-2 flex flex-row gap-3 last:mb-2 md:last:mb-6 lg:mx-auto lg:max-w-3xl">
+      <div class="relative flex h-full flex-1 md:flex-col">
+        <div class="flex ml-1 mt-1.5 md:w-full md:m-auto md:mb-2 gap-0 md:gap-2 justify-center">
+          <button class="btn relative btn-neutral border-0 md:border">
+            <div class="flex w-full items-center justify-center gap-2">
+            </div>
+          </button>
+        </div>
+        <div class="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
+          <textarea tabindex="0" data-id="request-:Rdd6:-2" style="max-height: 200px; height: 24px; overflow-y: hidden;" rows="1" class="m-0 w-full resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0"></textarea>
+          <button class="absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent">
+            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 mr-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+              <line x1="22" y1="2" x2="11" y2="13"></line>
+              <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </form>
+    <div class="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
+      <a href="https://help.openai.com/en/articles/6825453-chatgpt-release-notes" target="_blank" rel="noreferrer" class="underline">ChatGPT Feb 13 Version</a>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  box-sizing: border-box;
+  padding: 0;
+  border: none;
+  outline: none;
+  resize: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,67 @@
 <template>
-  <div class="bg-green-100 min-h-screen">
+  <div class="bg-green-100 min-h-screen" style="background-image: url('https://www.notion.so/images/page-cover/met_william_morris_1877_willow.jpg')">
     <!-- 页眉 -->
-    <header class="bg-white shadow-lg py-4">
-      <div class="container mx-auto px-4 md:px-8">
-        <h1 class="text-2xl font-bold text-gray-800">Chat App</h1>
+    <header class="flex items-center bg-white shadow-lg py-4 bg-gradient-to-r from-#b1b887 to-#b1cc8c bg-opacity-25 backdrop-blur-lg">
+      <div class="px-4 md:px-8">
+        <h1 class="text-2xl font-bold text-gray-800">ChatGPT</h1>
+      </div>
+      <div class="w-60">
+        <Listbox v-model="selectedPerson">
+          <div class="relative mt-1">
+            <ListboxButton
+              class="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+            >
+              <span class="block truncate">{{ selectedPerson.name }}</span>
+              <span
+                class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+              >
+                <ChevronUpDownIcon
+                  class="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </ListboxButton>
+
+            <transition
+              leave-active-class="transition duration-100 ease-in"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0"
+            >
+              <ListboxOptions
+                class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              >
+                <ListboxOption
+                  v-slot="{ active, selected }"
+                  v-for="person in people"
+                  :key="person.name"
+                  :value="person"
+                  as="template"
+                >
+                  <li
+                    :class="[
+                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900',
+                      'relative cursor-default select-none py-2 pl-10 pr-4',
+                    ]"
+                  >
+                    <span
+                      :class="[
+                        selected ? 'font-medium' : 'font-normal',
+                        'block truncate',
+                      ]"
+                      >{{ person.name }}</span
+                    >
+                    <span
+                      v-if="selected"
+                      class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                    >
+                      <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                    </span>
+                  </li>
+                </ListboxOption>
+              </ListboxOptions>
+            </transition>
+          </div>
+        </Listbox>
       </div>
     </header>
     
@@ -11,50 +69,60 @@
     <div class="container mx-auto px-4 md:px-8 mt-8">
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-          <h2 class="text-xl font-bold text-gray-800 mb-4">Chat Room 1</h2>
+          <h2 class="text-xl font-bold text-gray-800 mb-4">Shortcut</h2>
           <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin feugiat euismod dolor, ac suscipit libero commodo eget. Ut porta aliquet massa at elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.</p>
-          <a href="#" class="block mt-4 text-green-500 font-bold hover:underline">Join Room</a>
+          <!-- <a href="#" class="block mt-4 text-green-500 font-bold hover:underline"><NuxtLink to="/">Home page</NuxtLink></a> -->
+          <NuxtLink class="block mt-4 text-green-500 font-bold hover:underline" to="/chat">Try ChatGPT</NuxtLink>
+          
         </div>
         <div class="bg-white rounded-lg shadow-md p-6">
-          <h2 class="text-xl font-bold text-gray-800 mb-4">Chat Room 2</h2>
+          <h2 class="text-xl font-bold text-gray-800 mb-4">Shortcut</h2>
           <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin feugiat euismod dolor, ac suscipit libero commodo eget. Ut porta aliquet massa at elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.</p>
-          <a href="#" class="block mt-4 text-green-500 font-bold hover:underline">Join Room</a>
+          <a href="#" class="block mt-4 text-green-500 font-bold hover:underline">Try ChatGPT</a>
         </div>
         <div class="bg-white rounded-lg shadow-md p-6">
-          <h2 class="text-xl font-bold text-gray-800 mb-4">Chat Room 3</h2>
+          <h2 class="text-xl font-bold text-gray-800 mb-4">Shortcut</h2>
           <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin feugiat euismod dolor, ac suscipit libero commodo eget. Ut porta aliquet massa at elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.</p>
-          <a href="#" class="block mt-4 text-green-500 font-bold hover:underline">Join Room</a>
+          <a href="#" class="block mt-4 text-green-500 font-bold hover:underline">Try ChatGPT</a>
         </div>
       </div>
     </div>
     
     <!-- 聊天框 -->
-    <div class="container mx-auto px-4 md:px-8 mt-8">
+    <!-- <div class="container h-full mx-auto px-4 md:px-8 mt-8">
       <div class="bg-white rounded-lg shadow-md p-6">
-        <h2 class="text-xl font-bold text-gray-800"></h2>
-        <div class="overflow-y-scroll max-h-64">
-          <div class="flex flex-col space-y-2">
-            <div class="bg-gray-200 rounded-lg p-2">
-              <p class="text-gray-700">User 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div class="bg-gray-200 rounded-lg p-2">
-              <p class="text-gray-700">User 2: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <div class="bg-gray-200 rounded-lg p-2">
-              <p class="text-gray-700">User 1: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-          </div>
-        </div>
+        <div class="h-72"></div>
+      
         <div class="flex mt-4">
           <input type="text" class="border border-gray-400 rounded-md p-2 w-full mr-2" placeholder="Type your message...">
           <button class="bg-green-500 text-white rounded-md py-2 px-4">Send</button>
         </div>
       </div>
-    </div>
+    </div> -->
+    
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import {
+  Listbox,
+  ListboxLabel,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+
+const people = [
+  { name: 'Wade Cooper' },
+  { name: 'Arlene Mccoy' },
+  { name: 'Devon Webb' },
+  { name: 'Tom Cook' },
+  { name: 'Tanya Fox' },
+  { name: 'Hellen Schmidt' },
+]
+const selectedPerson = ref(people[0])
+
 
 </script>
