6239082d3478ef6ec1ba6b8009830eb3c4ee66a7
Capchen <chenjian@gyenno.com>
2023-03-14 10:00:06 +0800

    feat: 增加数据文件

---
+++ assets/tag.js
@@ -0,0 +1,245 @@
+export const Tags = {
+  favorite: {
+    label: translate({ message: "常用" }),
+    description: translate({
+      message: "常用/收藏",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#e9669e",
+  },
+
+  mind: {
+    label: translate({ message: "发散思维" }),
+    description: translate({
+      message: "质疑、思考、联想、归纳",
+      id: "showcase.tag.mind.description",
+    }),
+    color: "#86699e",
+  },
+
+  write: {
+    label: translate({ message: "写作辅助" }),
+    description: translate({
+      message: "写作辅助",
+      id: "showcase.tag.write.description",
+    }),
+    color: "#75581e",
+  },
+
+  article: {
+    label: translate({ message: "文章/故事" }),
+    description: translate({
+      message: "文章/故事",
+      id: "showcase.tag.article.description",
+    }),
+    color: "#dfd545",
+  },
+
+  text: {
+    label: translate({ message: "文本/词语" }),
+    description: translate({
+      message: "文本/词语",
+      id: "showcase.tag.text.description",
+    }),
+    color: "#554236",
+  },
+
+  seo: {
+    label: translate({ message: "SEO" }),
+    description: translate({
+      message: "文本/关键词拓展生成",
+      id: "showcase.tag.seo.description",
+    }),
+    color: "#BC9F77",
+  },
+
+  comments: {
+    label: translate({ message: "点评/评鉴" }),
+    description: translate({
+      message: "点评/评鉴",
+      id: "showcase.tag.comments.description",
+    }),
+    color: "#eeef99",
+  },
+
+  code: {
+    label: translate({ message: "IT/编程" }),
+    description: translate({
+      message:
+        "虽然说是编程，但 ChatGPT 是按照要求给出编程步骤的建议或简单的样例代码，这些代码只能作为测试，仍然需要专业程序员来修改。",
+      id: "showcase.tag.code.description",
+    }),
+    color: "#8c2f00",
+  },
+
+  ai: {
+    label: translate({ message: "AI" }),
+    description: translate({
+      message: "AI",
+      id: "showcase.tag.ai.description",
+    }),
+    color: "#ff6100", // Facebook blue
+  },
+
+  life: {
+    label: translate({ message: "自助百科" }),
+    description: translate({
+      message: "生活知识/自助百科",
+      id: "showcase.tag.life.description",
+    }),
+    color: "#a44fb7",
+  },
+
+  living: {
+    label: translate({ message: "生活质量" }),
+    description: translate({
+      message: "健身、营养、厨师、化妆、造型",
+      id: "showcase.tag.living.description",
+    }),
+    color: "#cf92e0",
+  },
+
+  interesting: {
+    label: translate({ message: "趣味知识" }),
+    description: translate({
+      message: "趣味知识",
+      id: "showcase.tag.interesting.description",
+    }),
+    color: "#127f82",
+  },
+
+  speech: {
+    label: translate({ message: "辩论/演讲" }),
+    description: translate({
+      message: "辩论/演讲",
+      id: "showcase.tag.speech.description",
+    }),
+    color: "#9c1d10",
+  },
+
+  social: {
+    label: translate({ message: "心理/社交" }),
+    description: translate({
+      message: "心理/社交",
+      id: "showcase.tag.social.description",
+    }),
+    color: "#ee3308",
+  },
+
+  philosophy: {
+    label: translate({ message: "哲学/宗教" }),
+    description: translate({
+      message: "哲学/宗教",
+      id: "showcase.tag.philosophy.description",
+    }),
+    color: "#ffcaa9",
+  },
+
+  teacher: {
+    label: translate({ message: "学术/老师" }),
+    description: translate({
+      message: "学术/老师",
+      id: "showcase.tag.teacher.description",
+    }),
+    color: "#fe6829",
+  },
+
+  games: {
+    label: translate({ message: "游戏" }),
+    description: translate({
+      message: "游戏",
+      id: "showcase.tag.games.description",
+    }),
+    color: "#574C57",
+  },
+
+  tool: {
+    label: translate({ message: "工具" }),
+    description: translate({
+      message: "工具",
+      id: "showcase.tag.tool.description",
+    }),
+    color: "#4267b2", // Facebook blue
+  },
+
+  interpreter: {
+    label: translate({ message: "终端/解释器" }),
+    description: translate({
+      message: "终端/解释器",
+      id: "showcase.tag.interpreter.description",
+    }),
+    color: "#ffa11c", // Facebook blue
+  },
+
+  language: {
+    label: translate({ message: "语言/翻译" }),
+    description: translate({
+      message: "语言/翻译",
+      id: "showcase.tag.language.description",
+    }),
+    color: "#39ca30",
+  },
+
+  company: {
+    label: translate({ message: "企业职位" }),
+    description: translate({
+      message: "一般企业内的岗位",
+      id: "showcase.tag.company.description",
+    }),
+    color: "#124f4e",
+  },
+
+  doctor: {
+    label: translate({ message: "医生" }),
+    description: translate({
+      message: "医生",
+      id: "showcase.tag.doctor.description",
+    }),
+    color: "#0e7774",
+  },
+
+  finance: {
+    label: translate({ message: "金融顾问" }),
+    description: translate({
+      message: "金融行业的岗位",
+      id: "showcase.tag.finance.description",
+    }),
+    color: "#14cfc3",
+  },
+
+  music: {
+    label: translate({ message: "音乐" }),
+    description: translate({
+      message: "音乐",
+      id: "showcase.tag.music.description",
+    }),
+    color: "#5cecdc",
+  },
+
+  professional: {
+    label: translate({ message: "行业顾问" }),
+    description: translate({
+      message: "律师、茶艺师等需要专业知识的岗位",
+      id: "showcase.tag.professional.description",
+    }),
+    color: "#ffcfc3",
+  },
+
+  contribute: {
+    label: translate({ message: "投稿" }),
+    description: translate({
+      message: "来自大家的投稿作品，能帮助我们开拓思路",
+      id: "showcase.tag.contribute.description",
+    }),
+    color: "#cecd21",
+  },
+
+  personal: {
+    label: translate({ message: "Personal" }),
+    description: translate({
+      message: "个人非生产力使用，仅作参考",
+      id: "showcase.tag.personal.description",
+    }),
+    color: "#ebcb63",
+  },
+};
